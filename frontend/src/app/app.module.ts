57a0c5a97b4d27d461770f200d7aa6481ff0cabd
carina0801 <83816251+carina0801@users.noreply.github.com>
2024-03-08 21:01:46 +0200

    header, page1 html, css

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Page1Component } from './components/page1/page1.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Page1Component
+    Page1Component,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
