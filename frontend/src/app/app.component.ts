950eb6faf656edafcb2caeae748da7322031b253
beni <szekrenyes.beni@gmail.com>
2024-03-08 21:09:17 +0200

    added page 2

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ViewContainerRef } from '@angular/core';
+import { Page2Component } from './components/page2/page2.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+  @ViewChild('container', { read: ViewContainerRef }) container!: ViewContainerRef;
+
+  constructor() {
+    this.container = {} as ViewContainerRef;
+  }
+
+  addDynamicComponent() {
+    const componentRef = this.container.createComponent(Page2Component);
+    // You can interact with the component's instance
+    // componentRef.instance.someProperty = 'someValue';
+  }
+
+  removeDynamicComponent() {
+    this.container.clear(); // This removes all components from the container
+  }
 }
