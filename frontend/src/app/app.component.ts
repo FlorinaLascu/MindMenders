dfc7dc4b360882a0b782401f71e88d5fc37657f1
beni <szekrenyes.beni@gmail.com>
2024-03-09 19:06:35 +0200

    some changes

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component, ViewChild, ViewContainerRef } from '@angular/core';
 import { Page1Component } from './components/page1/page1.component';
 import { Page2Component } from './components/page2/page2.component';
 import { Page3Component } from './components/page3/page3.component';
-import { Page4Component } from './components/page4/page4.component';
 
 @Component({
   selector: 'app-root',
